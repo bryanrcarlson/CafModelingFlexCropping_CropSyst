3db9aba8958a2de1c994de8f0a6deed1566cd7d8
Bryan Carlson <bryan.carlson@ars.usda.gov>
2017-03-14 16:29:55 -0700

    Deleted old param files after moving them to uppler level dir, also added a CS_control to specify sim year since I removed emulate folder

--- Kaur/ModelAssessment/setup.sh
+++ Kaur/ModelAssessment/setup.sh
@@ -5,6 +5,8 @@ if [ ! -d "Scenarios" ]; then
 fi
 pushd Scenarios
 ln -s ../../../ScenariosSimple/generate.sh generate.sh
+ln -s ../../../ScenariosSimple/year.CS_control year.CS_control
 chmod uga+rwx generate.sh
+chmod uga+rwx year.CS_control
 cp -r ../../../ScenariosSimple/rotations rotations
 popd
--- ScenariosSimple/generate.sh
+++ ScenariosSimple/generate.sh
@@ -1,3 +1,3 @@
 target_dir=$(date +"%y-%m-%d")
-generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=rotations  --ultimates=all
+generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=rotations  --ultimates=ultimates
 #--response=generator.response

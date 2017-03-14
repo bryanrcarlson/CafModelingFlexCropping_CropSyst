881152f67d44d48e83053d8caf92e7fd1dc6f6f4
Bryan Carlson <bryan.carlson@ars.usda.gov>
2017-03-14 16:49:45 -0700

    Made setup.sh create symbolic links to dbf and CS_control files

--- Kaur/ModelAssessment/setup.sh
+++ Kaur/ModelAssessment/setup.sh
@@ -5,8 +5,11 @@ if [ ! -d "Scenarios" ]; then
 fi
 pushd Scenarios
 ln -s ../../../ScenariosSimple/generate.sh generate.sh
-ln -s ../../../ScenariosSimple/year.CS_control year.CS_control
 chmod uga+rwx generate.sh
-chmod uga+rwx year.CS_control
 cp -r ../../../ScenariosSimple/rotations rotations
 popd
+
+ln -s ../../ScenariosSimple/year.CS_control year.CS_control
+chmod uga+rwx year.CS_control
+ln -s ../../Dbfs/Wsu-research-sites.dbf Wsu-research-sites.dbf
+chmod uga+rwx Wsu-research-sites.dbf

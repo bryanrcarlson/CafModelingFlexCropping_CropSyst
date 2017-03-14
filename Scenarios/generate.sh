de92ec12246a396a98ebc07ea34d1ca59e67f483
Bryan Carlson <bryan.carlson@ars.usda.gov>
2017-03-14 09:06:53 -0700

    Added ultimates param to generate file

--- Scenarios/generate.sh
+++ Scenarios/generate.sh
@@ -1,3 +1,3 @@
 target_dir=$(date +"%y-%m-%d")
-generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=anthrome --decode=decode
+generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=anthrome --decode=decode  --ultimates=all
 #--response=generator.response
--- ScenariosSimple/generate.sh
+++ ScenariosSimple/generate.sh
@@ -1,3 +1,3 @@
 target_dir=$(date +"%y-%m-%d")
-generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=rotations
+generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=rotations  --ultimates=all
 #--response=generator.response

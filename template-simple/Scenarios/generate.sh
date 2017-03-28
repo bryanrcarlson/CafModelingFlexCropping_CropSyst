ed014747cf809feb0a0ebf224b073d4ee2e71512
Bryan Carlson <bryan.carlson@ars.usda.gov>
2017-03-28 16:16:11 -0700

    Automated process of creating a script to run all scenarios

--- template-simple/Scenarios/generate.sh
+++ template-simple/Scenarios/generate.sh
@@ -1,3 +1,7 @@
 target_dir=$(date +"%y-%m-%d")
 generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --decode=  --template=rotations  --ultimates=ultimates
+
+pushd $target_dir
+cat ultimates | awk '{print "pushd " $0; print "CropSyst_5 --verbose=15"; print "popd" }'  > run_all.sh
+popd
 #--response=generator.response

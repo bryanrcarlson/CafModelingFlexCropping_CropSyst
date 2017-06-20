2e730fa5cec3f0a368c597695d865df692fb0277
Bryan Carlson <brcarlson@s58089071.temp.wsu.edu>
2017-06-20 09:03:51 -0700

    Updated template-simple with changes made to Kaur

--- Kaur/ModelAssessment/CorroborationYield/setup.sh
+++ Kaur/ModelAssessment/CorroborationYield/setup.sh
@@ -4,10 +4,53 @@ if [ ! -d "Scenarios" ]; then
     mkdir Scenarios
 fi
 pushd Scenarios
-ln -s ../../../Scenarios/generate.sh generate.sh
-chmod uga+rwx generate.sh
-cp -r ../../../Scenarios/rotations rotations
+    ln -s ../../../Scenarios/generate.sh generate.sh
+    chmod uga+rwx generate.sh
+    #cp -r ../../../Scenarios/rotations rotations
+    mkdir rotations
+    pushd rotations
+        mkdir sC-wW
+        mkdir sF-wW
+        #mkdir sP-wW
+        #mkdir sW-wW
+    popd
 popd
+
+# Create database files
+if [ ! -d "Database" ]; then
+    mkdir Database
+    pushd Database
+        mkdir Simulation
+        pushd Simulation
+            mkdir emulate
+            pushd emulate
+                echo "[simulation]" >> 2010-2011.CS_control
+                echo "start_date=2010244" >> 2010-2011.CS_control
+                echo "stop_date=2011273" >> 2010-2011.CS_control
+                
+                echo "[simulation]" >> 2011-2012.CS_control
+                echo "start_date=2011244" >> 2011-2012.CS_control
+                echo "stop_date=2012273" >> 2011-2012.CS_control
+                
+                echo "[simulation]" >> 2012-2013.CS_control
+                echo "start_date=2012244" >> 2012-2013.CS_control
+                echo "stop_date=2013273" >> 2012-2013.CS_control
+                
+                echo "[simulation]" >> 2013-2014.CS_control
+                echo "start_date=2013244" >> 2013-2014.CS_control
+                echo "stop_date=2014273" >> 2013-2014.CS_control
+                
+                echo "[simulation]" >> 2014-2015.CS_control
+                echo "start_date=2014244" >> 2014-2015.CS_control
+                echo "stop_date=2015273" >> 2014-2015.CS_control
+                
+                echo "[simulation]" >> 2015-2016.CS_control
+                echo "start_date=2015244" >> 2015-2016.CS_control
+                echo "stop_date=2016273" >> 2015-2016.CS_control
+            popd
+        popd
+    popd
+fi
 
 ln -s ../../Dbfs/corroboration_locations_72.dbf corroboration_locations_72.dbf
 chmod uga+rwx corroboration_locations_72.dbf

ad6e9ddb90273301f16574ab9e28fbde3c3eaf26
Bryan Carlson <brcarlson@s58089071.temp.wsu.edu>
2017-06-20 09:04:56 -0700

    Removed tempalate-aec since we have no plans to use anymore, renamed template-simple to just template

--- template-aec/Kaur/setup.bat
+++
@@ -1,6 +0,0 @@
-REM setup for Windows
-MKDIR Scenarios
-MKLINK generate.bat ..\Scenarios\generate.bat
-MKDIR Scenarios\anthrome
-XCOPY /E ..\Scenarios\anthrome Scenarios\anthrome
-
--- template-aec/Scenarios/generate.sh
+++
@@ -1,3 +0,0 @@
-target_dir=$(date +"%y-%m-%d")
-generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --template=anthrome --decode=decode  --ultimates=ultimates
-#--response=generator.response

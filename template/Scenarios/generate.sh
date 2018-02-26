be61a06f140b58375579f9ef8683975f126b7698
Bryan Carlson <bryan.r.carlson@gmail.com>
2018-02-26 15:57:39 -0800

    clean up generat script

--- template/Scenarios/generate.sh
+++ template/Scenarios/generate.sh
@@ -1,3 +1,3 @@
 target_dir=$(date +"%y-%m-%d")
 mkdir $target_dir
-generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition  --decode=  --template=rotations --ultimates=
+generator --verbose=255  --target=$target_dir  --recognition=scenario_level.recognition --template=rotations
